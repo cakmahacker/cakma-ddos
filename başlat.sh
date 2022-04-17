0e49ac4c90d69b6a6bb6ba6d10d626095ffad5b3
cakma_hack3r <91216396+cakmahacker@users.noreply.github.com>
2022-04-17 18:41:10 +0300

    Update başlat.sh

--- başlat.sh
+++ başlat.sh
@@ -1,9 +1,7 @@
 clear
 printf "\e[30m\e[40m[\e[40m\e[32m...\e[30m\e[40m]\e[40m\e[31m Pyfiglet\e[37m paket kurulumu...\n\n"
-sleep 3
 pip install pyfiglet
 printf "\e[30m\e[40m[\e[40m\e[32m...\e[30m\e[40m]\e[40m\e[31m Python\e[37m paket kurulumu...\n\n"
-sleep 3
 pkg install python3
 pyfiglet Yonlendirilir...
 sleep 3
